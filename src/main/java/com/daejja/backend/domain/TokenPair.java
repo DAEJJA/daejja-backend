02a81345ad4bec5c3bf9e0ecb9ee9be0880bb282
ye0ngeun <duddms2415@naver.com>
2023-10-24 02:45:24 +0900

    feat: token pair 도메인 구현

---
+++ src/main/java/com/daejja/backend/domain/TokenPair.java
@@ -0,0 +1,40 @@
+package com.daejja.backend.domain;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class TokenPair {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "token_pair_id")
+    private Long id;
+
+    private String accessToken;
+    private String refreshToken;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Builder
+    public TokenPair(String accessToken, String refreshToken, User user) {
+        this.accessToken = accessToken;
+        this.refreshToken = refreshToken;
+        this.user = user;
+    }
+
+    public static TokenPair createTokenPair(String accessToken, String refreshToken, User user) {
+        return TokenPair.builder()
+                .accessToken(accessToken)
+                .refreshToken(refreshToken)
+                .user(user)
+                .build();
+    }
+
+}
