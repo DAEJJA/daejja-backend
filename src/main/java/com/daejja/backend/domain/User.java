cd5f6bf5e983af941f6e5914118bd6219c6b3ae7
ye0ngeun <duddms2415@naver.com>
2023-10-24 02:41:47 +0900

    feat: user 도메인 구현

---
+++ src/main/java/com/daejja/backend/domain/User.java
@@ -0,0 +1,21 @@
+package com.daejja.backend.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long id;
+
+    private String loginId;
+    private String password;
+    private String nickName;
+
+}
